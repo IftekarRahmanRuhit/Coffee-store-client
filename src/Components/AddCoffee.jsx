b6b1fe5e5c192d68886592a2a274a33c8bacbeae
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-12-01 17:09:25 +0600

    initial setup complete

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            This is AddCoffee route
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            This is UpdateCoffee route
+        </div>
+    );
+};
+
+export default UpdateCoffee;
